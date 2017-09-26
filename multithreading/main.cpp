13156c45900ffd3cc57020b0c01bae8668f5dfde
Esteban Herrera <esteban@driveshop.com>
2017-09-25 21:01:39 -0700

    single async thread

--- multithreading/main.cpp
+++ multithreading/main.cpp
@@ -13,11 +13,16 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <thread>
+#include <unistd.h>
 
 void makeSale(int);
 
 using namespace std;
 int inventory = 15;
+const int min_wait = 500000;
+const int max_wait = 2000000;
+
 /*
  * 
  */
@@ -25,7 +30,8 @@ int main(int argc, char** argv) {
     cout << "Global Before Items: " << inventory << "\n";
 
     // Make a sale
-    makeSale(2);
+    thread t1(makeSale, 2);    
+    t1.join();
     
     cout << "Global After Items: " << inventory << "\n";
     
@@ -34,15 +40,21 @@ int main(int argc, char** argv) {
 
 void makeSale(int itemsToSell) {
     int available;
+    int timeToWait;
+    std::thread::id this_id = std::this_thread::get_id();
     do {
-        cout << "Attempting to sell " << itemsToSell << " items\n";
+        timeToWait = min_wait + (rand() % static_cast<int>(max_wait - min_wait + 1));
+        
+        cout << "(" << this_id << ") " << "Attempting to sell " << itemsToSell << " items\n";
         available = inventory;
-        cout << "Before sale Items: " << available << "\n";
+        cout << "(" << this_id << ") " << "Before sale Items: " << available << "\n";
         available -= itemsToSell;
-        cout << "Salesman x is making a sale of " << itemsToSell << " items\n";        
+        cout << "(" << this_id << ") " << "Waiting " << timeToWait << " micro seconds\n";
+        usleep(timeToWait);
+        cout << "(" << this_id << ") " << "Salesman x is making a sale of " << itemsToSell << " items\n";        
         inventory = available;
-        cout << "After sale Items: " << available << "\n";
+        cout << "(" << this_id << ") " << "After sale Items: " << available << "\n";
     } while (available >= itemsToSell);
-    cout << "Done making sales\n";
+    cout << "(" << this_id << ") " << "Done making sales\n";
 }
 
